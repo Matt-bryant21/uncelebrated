4614e776f2063ec9262bb10fd41d58c58c3d1bb1
Matt-bryant21 <14771@coderacademy.edu.au>
2024-09-21 15:32:52 +1000

    installed tailwind

---
+++ Uncelebrated/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Main />
+    </Router>
+  );
+}
+
+export default App
